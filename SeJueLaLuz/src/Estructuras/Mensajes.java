2d9f6610dfec97fb6b171d7222582ef48727b6cb
natalyablanco <natalyablanco@e41c7a16-7ff5-9753-f35a-a1f38d0a47f4>
2011-07-12 19:54:08 +0000

    clase para manejar los mensajes multicast enviados

---
+++ SeJueLaLuz/src/Estructuras/Mensajes.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Estructuras;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author natalya
+ */
+public class Mensajes implements Runnable{
+
+    private String msj = "";
+
+    private String respuesta = "";
+
+    private infoRed inf;
+
+    Multicast multi = new Multicast();
+
+
+    public Mensajes(String mensaje, MulticastSocket socket) {
+        msj = mensaje;
+    }
+
+
+    public void run() {
+        
+        if(msj.equals("Activo?")){
+            respuesta = "Estoy activo ip: " + inf.miIp() + " host: "+ inf.miHost();
+            System.out.println("Esta es la respuesta "+ respuesta);
+            
+            multi.enviarMensaje(respuesta);
+          
+
+         }else if(msj.equals("Actualice")){
+
+
+         }
+        
+    }
+
+
+}
