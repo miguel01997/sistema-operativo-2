3278626cee587289b0a5a7fffe7804efa8d61f61
darknessl89@gmail.com <darknessl89@gmail.com@e41c7a16-7ff5-9753-f35a-a1f38d0a47f4>
2011-07-16 22:26:58 +0000

    min servidores 1

--- trunk/SeJueLaLuz/src/Estructuras/Config.java
+++ trunk/SeJueLaLuz/src/Estructuras/Config.java
@@ -28,7 +28,7 @@ public class Config {
     
     
     /*Numero de servidores que van a ejecutar la clase*/
-    public final static int numServidores = 2;
+    public final static int numServidores = 1;
     
     /**Ejecuta la aplicacion asi no haya el numero minimo de servidores 
      * disponibles es decir numServidres > numero de servidores conectados*/
